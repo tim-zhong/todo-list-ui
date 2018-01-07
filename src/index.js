91dee48e620820f802b2bc520c5548568d9d2e88
Runbiao Zhong <runbiaozhong@Runbiaos-Air.attlocal.net>
2018-01-07 12:38:43 -0800

    add initial code

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
+
+render(
+	<Provider store= {store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import todos from './todo.js'
+import visibilityFilter from './visibilityFilter.js'
+
+const todoApp = combineReducers({
+	todos: todos,
+	visibilityFilter: visibilityFilter
+});
+
+export default todoApp
